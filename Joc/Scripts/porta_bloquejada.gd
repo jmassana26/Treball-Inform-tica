955852f9d75d645b66baf6c68cd9ff8d4d4828c6
jmassana26 <60349718+jmassana26@users.noreply.github.com>
2020-06-09 11:09:17 +0200

    Add files via upload
    porta blava arreglada

--- Joc/Scripts/porta_bloquejada.gd
+++ Joc/Scripts/porta_bloquejada.gd
@@ -6,7 +6,7 @@ var clau1 = false
 var sala1
 func _on_clau_blava_body_entered(body):
 	if body.get_name() == "personatge" and clau1 == true:
-		pass
+		$CollisionShape2D.set_deferred("disabled", true)
 		
 		
   
